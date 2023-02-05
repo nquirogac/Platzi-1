a63099e4ffe4978cde00417068027fdb0a830e67
nquirogac <nquirogac@unal.edu.co>
2023-02-04 21:13:34 -0500

    product detail

--- main.js
+++ main.js
@@ -3,7 +3,7 @@ const desktopMenu = document.querySelector('.desktop-menu');
 const menuHamIcon = document.querySelector('.menu');
 const mobileMenu = document.querySelector('.mobile-menu');
 const menuCarritoIcon = document.querySelector('.navbar-shopping-cart');
-const aside = document.querySelector('.product-detail');
+const shoppingCartContainer = document.querySelector('#shoppingCartContainer');
 const cardsContainer = document.querySelector('.cards-container');
 
 menuEmail.addEventListener('click', toggleDesktopMenu);
@@ -11,19 +11,19 @@ menuHamIcon.addEventListener('click', toggleMobileMenu);
 menuCarritoIcon.addEventListener('click', toggleCarritoMenu);
 
 function toggleDesktopMenu(){
-    const isAsideMenuOpen = !aside.classList.contains('inactive');
+    const isAsideMenuOpen = !shoppingCartContainer.classList.contains('inactive');
     
     if (isAsideMenuOpen) {
-        aside.classList.toggle('inactive');
+        shoppingCartContainer.classList.toggle('inactive');
     }
     desktopMenu.classList.toggle('inactive');
 }
 
 function toggleMobileMenu(){
-    const isAsideMenuOpen = !aside.classList.contains('inactive');
+    const isAsideMenuOpen = !shoppingCartContainer.classList.contains('inactive');
     
     if (isAsideMenuOpen) {
-        aside.classList.toggle('inactive');
+        shoppingCartContainer.classList.toggle('inactive');
     }
     mobileMenu.classList.toggle('inactive');
 }
@@ -37,7 +37,7 @@ function toggleCarritoMenu(){
     if (isDesktopMenuOpen) {
         desktopMenu.classList.toggle('inactive');
     }
-    aside.classList.toggle('inactive');    
+    shoppingCartContainer.classList.toggle('inactive');    
 }
 
 const productList = [];
